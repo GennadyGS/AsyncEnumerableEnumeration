3e1b1ac7ee0e3c0cc5b6233c0ee788da69b21209
Gennadii Saltyshchak <GennadyGS@gmail.com>
2024-02-26 17:41:18 +0200

    Add project files.

---
+++ Benchmarks.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace AsyncEnumerableEnumeration;
+
+[LongRunJob]
+public class Benchmarks
+{
+    const int ItemCount = 1_000_000;
+
+    private readonly IAsyncEnumerable<int> _asyncEnumerable =
+        System.Linq.AsyncEnumerable.Range(1, ItemCount);
+
+    private readonly IEnumerable<int> _enumerable = System.Linq.Enumerable.Range(1, ItemCount);
+
+
+    [Benchmark]
+    public async Task AsyncEnumerable()
+    {
+        await foreach (var _ in _asyncEnumerable)
+        {
+        }
+    }
+
+    [Benchmark]
+    public void Enumerable()
+    {
+        foreach (var _ in _enumerable)
+        {
+        }
+    }
+}
